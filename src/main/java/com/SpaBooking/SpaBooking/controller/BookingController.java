2ddd906fb7fbae0f6a7f7b347bc1c8f0ebdeff2b
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2024-04-22 17:52:02 +0200

    deploy

--- src/main/java/com/SpaBooking/SpaBooking/controller/BookingController.java
+++ src/main/java/com/SpaBooking/SpaBooking/controller/BookingController.java
@@ -2,7 +2,6 @@ package com.SpaBooking.SpaBooking.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
